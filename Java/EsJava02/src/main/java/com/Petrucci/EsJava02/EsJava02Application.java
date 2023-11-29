54e9cd7a6de29f001a8de315429b3cbbad19b114
Peppe1795 <petrucci238@gmail.com>
2023-11-29 17:01:37 +0100

    es java

---
+++ Java/EsJava02/src/main/java/com/Petrucci/EsJava02/EsJava02Application.java
@@ -0,0 +1,13 @@
+package com.Petrucci.EsJava02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsJava02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsJava02Application.class, args);
+	}
+
+}
