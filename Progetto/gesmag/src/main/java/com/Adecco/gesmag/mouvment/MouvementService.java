d98a09135675fe7d50dc86619fd210f32cb8c8c6
Peppe1795 <petrucci238@gmail.com>
2023-12-01 16:31:40 +0100

    create class serivce payload controller and repo

--- Progetto/gesmag/src/main/java/com/Adecco/gesmag/mouvment/MouvementService.java
+++ Progetto/gesmag/src/main/java/com/Adecco/gesmag/mouvment/MouvementService.java
@@ -29,35 +29,28 @@ public class MouvementService {
 
 	@Transactional
 	public Mouvement create(MouvementPayLoad body) {
-		// Verifica se l'ID del prodotto è presente nel corpo della richiesta
+
 		if (body.getProductId() == null) {
 			throw new IllegalArgumentException("L'ID del prodotto è obbligatorio nel corpo della richiesta.");
 		}
 
-		// Carica il Product all'interno del contesto di persistenza
 		Product product = pr.findById(body.getProductId())
 				.orElseThrow(() -> new NotFoundException("Product con ID " + body.getProductId() + " non trovato"));
 
-		// Verifica se il Product ha già un Mouvement associato
 		if (product.getMouvement() != null) {
-			// Gestisci la situazione in base ai requisiti del tuo sistema
-			// Puoi aggiornare il Mouvement esistente, lanciare un'eccezione, ecc.
+
 			throw new IllegalStateException(
 					"Il Product con ID " + body.getProductId() + " ha già un Mouvement associato.");
 		}
 
-		// Crea il Mouvement associandolo al Product
 		Mouvement mouvement = new Mouvement(body.getCodiceMovimento(), body.getNomeCliente(), product, body.getPrezzo(),
 				body.getQuantitaMoviementata());
 
-		// Salva il Mouvement
 		Mouvement savedMouvement = rp.save(mouvement);
 
-		// Aggiorna il prodotto con l'ID del movimento
 		product.setMouvement(savedMouvement);
 		pr.save(product);
 
-		// Restituisci il Mouvement salvato
 		return savedMouvement;
 	}
 
