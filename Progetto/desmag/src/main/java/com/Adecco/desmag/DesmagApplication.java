2bda45a3e76e13bb59a5a9b46c76b997b8afa0f3
Peppe1795 <petrucci238@gmail.com>
2023-12-01 12:01:06 +0100

    start

---
+++ Progetto/desmag/src/main/java/com/Adecco/desmag/DesmagApplication.java
@@ -0,0 +1,13 @@
+package com.Adecco.desmag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesmagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesmagApplication.class, args);
+	}
+
+}
---
+++ Progetto/desmag/src/test/java/com/Adecco/desmag/DesmagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Adecco.desmag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesmagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
