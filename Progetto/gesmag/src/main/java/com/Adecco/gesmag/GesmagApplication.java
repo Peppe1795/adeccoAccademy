3a034b50d7da35f2105b906d49c5782b60ab745b
Peppe1795 <petrucci238@gmail.com>
2023-12-01 12:27:30 +0100

    fix start

---
+++ Progetto/gesmag/src/main/java/com/Adecco/gesmag/GesmagApplication.java
@@ -0,0 +1,13 @@
+package com.Adecco.gesmag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesmagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GesmagApplication.class, args);
+	}
+
+}
